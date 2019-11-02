bccbba857de5e789e5dc7fe314ee3ab0957ff2bd
kkoukakis <kkoukakis@windowslive.com>
2019-11-02 12:40:50 +0200

    Problem 2

--- Problem2/Problem2.py
+++ Problem2/Problem2.py
@@ -1,14 +1,21 @@
-below = 4000000
-sum = 0
-fib_number = 1;
+below_number = 4000000
 
-for x in range (1,below):
-    if x % 2 != 0:
-        sum = sum + x
-    x = 
+even_sum = 1
+a = 0
+b = 1
+skip = False
 
+if below_number == 0:
+    print(a)
+    skip = True
 
-print(sum)
-
-
+if skip == False:
+    while (b <= below_number):
+        c = a + b
+        a = b
+        b = c 
+        if(b % 2 == 1 and b <= below_number):
+            even_sum = even_sum + b
+           #print(b)
+    print(even_sum)
 
