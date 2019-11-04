956051963062771c46860ea04676d7c2195bf9d9
kkoukakis <kkoukakis@windowslive.com>
2019-11-04 12:19:10 +0200

    corrections

---
+++ Problem7/lib.py
@@ -0,0 +1,13 @@
+#Project 7 Library
+import math
+
+def is_prime(num):
+    if num <= 1: return False
+    if num == 2: return True
+    if num % 2 == 0: return False
+    if num < 2: return False
+    root = int(math.sqrt(num)) + 1
+    # print('ROOT:' ,root)
+    for i in range(3,root,2):
+        if ( num % i ) == 0: return False
+    return True
