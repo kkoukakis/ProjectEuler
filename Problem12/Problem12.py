9ab3fc85eb5958e97ab3e315e3777a39af9a12c4
kkoukakis <kkoukakis@windowslive.com>
2019-11-06 20:06:33 +0200

    changes 11, 12

--- Problem12/Problem12.py
+++ Problem12/Problem12.py
@@ -1,29 +1,19 @@
-
-def has_over_factors(number,num_of_factors):
-    a = number
-    found = False
-    x = 1
-    factors = 0
-    while a % x == 0:
-        print(factors)
-        factors += 1
-        x += 1
-        if factors > 500:
-            found = True
-            break
-        if x > a: 
-            return 0
-    return a
+def has_over_factors(array,num_of_factors,length):
+    for j in range(1,length):
+        number = array[j-1]
+        factors = 0
+        for i in range(1, number):
+            if number % i == 0:
+               factors+=1
+            if factors > 500:
+                return number
+    return 0   
 
 done = False
-i = 500000
-while done is False:
-    a = int(i*(i+1)/2)
-    print("Number:",a)
-    result = has_over_factors(a,500)
-    print("Number_of_factors:",result)
-    if result > 0:
-        print(result)
-        break
-    i+=5
 
+array = []
+i = 1000
+while i in range(1000,10000):
+    array.append(int(i*(i+1)/2))
+    i+=1
+print(has_over_factors(array,500,len(array)))
